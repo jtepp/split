ca7e0b0ad988255ca679c33b8e69369b0e5e37e0
Jacob Tepperman <jtepp@icloud.com>
2021-05-26 17:10:29 -0400

    added image picker from tutorial

---
+++ split/Views/Images/ImagePicker.swift
@@ -0,0 +1,55 @@
+//
+//  ImagePicker.swift
+//  split
+//
+//  Created by Jacob Tepperman on 2021-05-26.
+//
+
+import SwiftUI
+
+class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @Binding var img: UIImage?
+    @Binding var isShown: Bool
+    
+    init(img: Binding<UIImage?>, isShown: Binding<Bool>) {
+        _img = img
+        _isShown = isShown
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            img = uiImage
+            isShown = false
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isShown = false
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIImagePickerController
+    typealias Coordinator = ImagePickerCoordinator
+    
+    @Binding var img: UIImage?
+    @Binding var isShown: Bool
+    var sourceType: UIImagePickerController.SourceType = .camera
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePickerCoordinator(img: $img, isShown: $isShown)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+}
+
+
--- split/Views/ProfileView.swift
+++ split/Views/ProfileView.swift
@@ -11,6 +11,9 @@ struct ProfileView: View {
     @Binding var house: House
     @Binding var m: Member
     @State var showSignOut = false
+    @State var showImagePicker = false
+    @State var sourceType: UIImagePickerController.SourceType = .camera
+    @State var img: UIImage?
     var body: some View {
         ScrollView {
             HeaderText(text: "Profile")
@@ -21,6 +24,36 @@ struct ProfileView: View {
                     .frame(width: 200, height: 200)
                     .clipShape(RoundedRectangle(cornerRadius: 25))
                     .shadow(radius: 4)
+                    .overlay(
+                        Menu(content: {
+                            Button(action: {
+                                showImagePicker = true
+                                sourceType = .camera
+                            }, label: {
+                                Text("Take Picture")
+                            })
+                            Button(action: {
+                                showImagePicker = true
+                                sourceType = .photoLibrary
+                                
+                                
+                            }, label: {
+                                Text("Choose from Library")
+                            })
+                            
+                        }, label: {
+                            Image(systemName: "camera.fill")
+                                .padding(10)
+                                .foregroundColor(.black)
+                                .background(
+                                        Circle()
+                                            .fill(
+                                                Color.gray
+                                            )
+                                )
+                        })
+                        .offset(y: 100)
+                    )
                 Text(m.name)
                     .font(.largeTitle)
                     .bold()
@@ -60,6 +93,11 @@ struct ProfileView: View {
             .padding(.vertical, 40)
             .foregroundColor(.white)
             .background(Color.black.edgesIgnoringSafeArea(.all))
+            .sheet(isPresented: $showImagePicker, content: {
+                ImagePicker(img: $img, isShown: $showImagePicker, sourceType: sourceType)
+            })
+            
+            
 //            .onAppear{
 //                UserDefaults.standard.set(m.name, forKey: "name")
 //                UserDefaults.standard.set(m.id, forKey: "myId")
@@ -68,3 +106,8 @@ struct ProfileView: View {
     }
 }
 
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView(house: .constant(.placeholder), m: .constant(.placeholder))
+    }
+}
