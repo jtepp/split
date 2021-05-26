1de94ec9502c1bfd9291a2aa71d72018e0fcc2a5
Jacob Tepperman <jtepp@icloud.com>
2021-05-26 18:16:54 -0400

    Added segmented picker to payment

--- split/Views/Payments/PaymentView.swift
+++ split/Views/Payments/PaymentView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct PaymentView: View {
+    @Binding var house: House
+    @State var typeSelection = 0
     var body: some View {
-        Text("pay")
-            .foregroundColor(.white)
+        ScrollView {
+            HeaderText(text: "Create")
+            Picker(selection: $typeSelection, label: Text("Picker"), content: {
+                Text("Payment").tag(0)
+                Text("Request").tag(1)
+            })
+            .pickerStyle(SegmentedPickerStyle())
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(
+                        Color.white.opacity(0.2)
+                    )
+            )
+            .padding()
+        }
     }
 }
 
 
+struct PaymentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            PaymentView(house: .constant(.placeholder))
+        }
+    }
+}
--- split/Views/Tabs/TabsView.swift
+++ split/Views/Tabs/TabsView.swift
@@ -18,7 +18,7 @@ struct TabsView: View {
                         .tag(0)
                     MembersView(house: $house)
                         .tag(1)
-                    PaymentView()
+                    PaymentView(house: $house)
                         .tag(2)
                     ProfileView(house: $house, m: .constant(house.members.first(where: { (m) -> Bool in
                         return m.id == myId
