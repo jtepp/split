1fd70e11973095001012226740d6a038a6d0910a
Jacob Tepperman <jtepp@icloud.com>
2021-05-26 15:59:22 -0400

    Added request functionality to member payment info view

--- split/Views/Members/MemberPaymentInfoView.swift
+++ split/Views/Members/MemberPaymentInfoView.swift
@@ -11,36 +11,42 @@ struct MemberPaymentInfoView: View {
     @Binding var member: Member
     @Binding var payments: [Payment]
     var body: some View {
-            VStack {
-                HStack {
-                    moneyText(b: $member.balance, pre: "Balance: ")
-                        .font(.headline)
-                    Spacer()
-                }
-                Rectangle()
-                    .fill(Color.black.opacity(0.5))
-                    .frame(height:2)
-                
-                ScrollView {
-                    ForEach(payments.filter{ p in
-                        return (p.to == member.name) || (p.from == member.name)
-                    }.sorted(by: { (a, b) -> Bool in
-                        return a.time > b.time
-                    })){ payment in
+        VStack {
+            HStack {
+                moneyText(b: $member.balance, pre: "Balance: ")
+                    .font(.headline)
+                Spacer()
+            }
+            Rectangle()
+                .fill(Color.black.opacity(0.5))
+                .frame(height:2)
+            
+            ScrollView {
+                ForEach(payments.filter{ p in
+                    return (p.to == member.name) || (p.from == member.name) || (p.reqfrom.contains(member.name))
+                }.sorted(by: { (a, b) -> Bool in
+                    return a.time > b.time
+                })){ payment in
+                    if payment.isRequest {
+                        GeneralRequestCell(payment: .constant(payment))
+                            .padding(.horizontal)
+                            .padding(.vertical, 5)
+                    } else {
                         GeneralPaymentCell(payment: .constant(payment))
                             .padding(.horizontal)
                             .padding(.vertical, 5)
                     }
-                
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(
-                                Color.black.opacity(0.5)
-                            )
-                )
                 }
+                
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(
+                            Color.black.opacity(0.5)
+                        )
+                )
             }
-            
+        }
+        
         .padding()
         .background(
             RoundedRectangle(cornerRadius: 20)
