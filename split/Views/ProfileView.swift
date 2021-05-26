acbcf3facac3f1deb384837cb043346492c85180
Jacob Tepperman <jtepp@icloud.com>
2021-05-26 14:02:10 -0400

    added signout button

--- split/Views/ProfileView.swift
+++ split/Views/ProfileView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ProfileView: View {
     @Binding var m: Member
+    @State var showSignOut = false
     var body: some View {
         ScrollView {
             HeaderText(text: "Profile")
@@ -26,13 +27,38 @@ struct ProfileView: View {
                     Text("House admin")
                 }
                 Spacer()
+                MemberPaymentInfoView(member: $m)
+                Spacer()
+                Button(action: {
+                    showSignOut = true
+                }, label: {
+                    HStack {
+                        Spacer()
+                        Text("Sign Out")
+                            .foregroundColor(.red)
+                        Spacer()
+                    }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(
+                                Color.white.opacity(0.2)
+                            )
+                    )
+                    .padding()
+                })
+                .alert(isPresented: $showSignOut, content: {
+                    Alert(title: Text("Sign Out"), message: Text("Please confirm you want to sign out of this account"), primaryButton: Alert.Button.destructive(Text("Confirm"), action: {
+                        //signout
+                    }), secondaryButton: Alert.Button.cancel())
+                })
                 Text("ID: \(m.id)")
                     .font(.caption)
                     .foregroundColor(Color("Secondary"))
-                Spacer()
-                MemberPaymentInfoView(member: $m)
             }
             .padding(.vertical, 40)
+            .foregroundColor(.white)
+            .background(Color.black.edgesIgnoringSafeArea(.all))
         }
     }
 }
