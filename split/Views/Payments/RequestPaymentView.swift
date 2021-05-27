53de456dd03b10a7b7155fd6499e4ef0bf8cd147
Jacob Tepperman <jtepp@icloud.com>
2021-05-26 21:18:39 -0400

    copied over request payment view

--- split/Views/Payments/RequestPaymentView.swift
+++ split/Views/Payments/RequestPaymentView.swift
@@ -9,10 +9,57 @@ import SwiftUI
 
 struct RequestPaymentView: View {
     @Binding var house: House
+    @State var showPicker = false
+    @State var choice: [Member] = [Member]()
+    @State var amountText = String()
+    @State var memoText = String()
     var body: some View {
-        Text("To:")
-            .font(.title)
-            .bold()
-            .foregroundColor(.white)
+        VStack {
+            HStack {
+                Text("To:")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                Spacer()
+                Button(action: {
+                    showPicker = true
+                }, label: {
+                    PickerButton(text: "Tap to Select", choice: $choice)
+                        .padding(10)
+                        .foregroundColor(.white)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(
+                                    Color.white.opacity(0.5)
+                                )
+                        )
+                })
+                Spacer()
+            }
+            .padding()
+            .sheet(isPresented: $showPicker, content: {
+                MemberPicker(show: $showPicker, house: $house, choice: $choice, multiple: false)
+            })
+//            Spacer()
+            InputField(name: "Amount", text: $amountText)
+            .padding()
+            InputField(name: "Memo", text: $memoText)
+                .padding()
+            Spacer()
+            Button(action: {}, label: {
+                HStack {
+                    Spacer()
+                    Text("Post")
+                        .foregroundColor(choice.isEmpty || amountText.isEmpty || !amountText.isNumeric ? .clear : .white)
+                    Spacer()
+                }
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(choice.isEmpty || amountText.isEmpty || !amountText.isNumeric ? .clear : Color.blue)
+                    )
+                    .padding()
+            })
+        }
     }
 }
