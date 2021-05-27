6d6056b7b9368b7143ec21e14253f4f78ad472ad
Jacob Tepperman <jtepp@icloud.com>
2021-05-27 10:44:28 -0400

    removed brackets and allowed 3rd person to be type checked

--- split/Views/Members/MemberPaymentInfoView.swift
+++ split/Views/Members/MemberPaymentInfoView.swift
@@ -23,25 +23,27 @@ struct MemberPaymentInfoView: View {
             
             ScrollView {
                 ForEach(house.members.filter({ (m) -> Bool in
-                    return (-(member.owesMe[m.name] ?? 0) + (member.iOwe[m.name] ?? 0)) != 0
+                    return ((member.iOwe[m.name] ?? 0) - (member.owesMe[m.name] ?? 0)) != 0
                 })) { m in
                     HStack {
-//                        //if member is myId
-//                        if member.id == UserDefaults.standard.string(forKey: "myId") {
-//                            Text((-(member.owesMe[m.name] ?? 0) + (member.iOwe[m.name] ?? 0)) > 0 ? "You owe \(m.name):" : "\(m.name) owes you:")
-//                        } else {
-//                            //if member is not myId but m is myId
-//                            if m.id == UserDefaults.standard.string(forKey: "myId") {
-//                                Text((-(member.owesMe[m.name] ?? 0) + (member.iOwe[m.name] ?? 0)) > 0 ? "\(member.name) owes you:" : "You owe \(member.name):")
-//                            } else {
-//                                //if member is not myId^ and m is not myId
-//                                Text((-(member.owesMe[m.name] ?? 0) + (member.iOwe[m.name] ?? 0)) > 0 ? "\(member.name) owes \(m.name):" : "\(m.name) owes \(member.name):")
-//                            }
+                        //if member is myId
+                        if checkWithMy(id: member.id) {
+                            Text((member.iOwe[m.name] ?? 0) - (member.owesMe[m.name] ?? 0) > 0 ? "You owe \(m.name):" : "\(m.name) owes you:")
+                        }
+                        else {
+                            //if member is not myId but m is myId
+                            if checkWithMy(id: m.id) {
+                                Text((member.iOwe[m.name] ?? 0) - (member.owesMe[m.name] ?? 0) > 0 ? "\(member.name) owes you:" : "You owe \(member.name):")
+                            }
+                            else {
+                                //if member is not myId^ and m is not myId
+                                Text((member.iOwe[m.name] ?? 0) - (member.owesMe[m.name] ?? 0) > 0 ? "\(member.name) owes \(m.name):" : "\(m.name) owes \(member.name):")
+                            }
 //                            
 //
-//                        }
+                        }
                         Spacer()
-                        Text("$\(abs((-(member.owesMe[m.name] ?? 0) + (member.iOwe[m.name] ?? 0))), specifier: "%.2f")")
+                        Text("$\(abs((member.iOwe[m.name] ?? 0) - (member.owesMe[m.name] ?? 0)), specifier: "%.2f")")
                     }
                     .padding(.horizontal)
                     .padding(.top, 10)
@@ -67,4 +69,7 @@ struct MemberPaymentInfoView: View {
         )
         .padding()
     }
+}
+func checkWithMy(id: String) -> Bool {
+    return id == UserDefaults.standard.string(forKey: "myId")
 }
