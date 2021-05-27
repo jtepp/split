925a4dbfe74d916993f546e080818ab9c40f6c40
Jacob Tepperman <jtepp@icloud.com>
2021-05-26 20:43:41 -0400

    fixed profile view to update live

--- split/Views/Tabs/TabsView.swift
+++ split/Views/Tabs/TabsView.swift
@@ -20,7 +20,7 @@ struct TabsView: View {
                         .tag(1)
                     PaymentView(house: $house)
                         .tag(2)
-                    ProfileView(house: $house, m: .constant(house.members.first(where: { (m) -> Bool in
+                    ProfileView(house: $house, m: .constant($house.members.wrappedValue.first(where: { (m) -> Bool in
                         return m.id == myId
                     }) ?? Member.empty))
                         .tag(3)
