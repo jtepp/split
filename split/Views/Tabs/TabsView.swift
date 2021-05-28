50d64482dcf9833fd489e2562bac67a2ba58342f
Jacob Tepperman <jtepp@icloud.com>
2021-05-28 11:52:14 -0400

    Revert "added another on change which will do nothing, made leave delete payments"
    This reverts commit 1fb91fee5eb6d529747e48e76f1aa1a1bbb5f9be.

--- split/Views/Tabs/TabsView.swift
+++ split/Views/Tabs/TabsView.swift
@@ -32,9 +32,6 @@ struct TabsView: View {
             .onAppear(){
                 Fetch().getHouse(h: $house, inWR: $inWR, noProf: $noProf)
             }
-            .onChange(of: house.members.count, perform: { (_) in
-                Fetch().getHouse(h: $house, inWR: $inWR, noProf: $noProf)
-            })
             .sheet(isPresented: $inWR, onDismiss: {
                 Fetch().getHouse(h: $house, inWR: $inWR, noProf: $noProf)
             }) {
