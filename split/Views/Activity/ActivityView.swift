98e47529027343d2918278abe2b460dba76e0795
Jacob Tepperman <jtepp@icloud.com>
2021-05-28 20:12:33 -0400

    dont go thru payments if ur not in the house

--- split/Views/Activity/ActivityView.swift
+++ split/Views/Activity/ActivityView.swift
@@ -36,73 +36,77 @@ struct ActivityView: View {
             ForEach(house.payments.sorted(by: { a, b in
                 return a.time > b.time
             })) { payment in
-                if payment.isAn {
-                    if house.members.first(where: { (m) -> Bool in
-                        return m.id == UserDefaults.standard.string(forKey: "myId")
-                    })?.admin ?? returnFalseAndWR(inWR: $inWR, h: $house, m: $m) {
-                        ActivityAnnouncementCell(payment: .constant(payment))
-                            .contextMenu(menuItems: {
-                                Button(action: {
-                                    Fetch().deletePayment(p: payment, h: house)
-                                }, label: {
-                                    Text("Delete")
-                                        .foregroundColor(.red)
-                                    Image(systemName: "trash.fill")
-                                        .foregroundColor(.red)
-                                    
-                                }
-                                )
-                            })
-                            .padding(.bottom, -20)
-                    } else {
-                        ActivityAnnouncementCell(payment: .constant(payment))
-                            .padding(.bottom, -20)
-                    }
-                } else if payment.isRequest {
-                    
-                    if payment.by == UserDefaults.standard.string(forKey: "myId") || house.members.first(where: { (m) -> Bool in
-                        return m.id == UserDefaults.standard.string(forKey: "myId")
-                    })!.admin {
-                        ActivityRequestCell(payment: .constant(payment))
-                            .contextMenu(menuItems: {
-                                Button(action: {
-                                    Fetch().deletePayment(p: payment, h: house)
-                                }, label: {
-                                    Text("Delete")
-                                        .foregroundColor(.red)
-                                    Image(systemName: "trash.fill")
-                                        .foregroundColor(.red)
-                                    
-                                }
-                                )
-                            })
-                            .padding(.bottom, -20)
-                    } else {
-                        ActivityRequestCell(payment: .constant(payment))
-                            .padding(.bottom, -20)
-                    }
-                } else {
-                    
-                    if payment.by == UserDefaults.standard.string(forKey: "myId") || house.members.first(where: { (m) -> Bool in
-                        return m.id == UserDefaults.standard.string(forKey: "myId")
-                    })!.admin {
+                if house.members.contains(where: { (m) -> Bool in
+                    return m.id == UserDefaults.standard.string(forKey: "myId")
+                }){
+                    if payment.isAn {
+                        if house.members.first(where: { (m) -> Bool in
+                            return m.id == UserDefaults.standard.string(forKey: "myId")
+                        })!.admin {
+                            ActivityAnnouncementCell(payment: .constant(payment))
+                                .contextMenu(menuItems: {
+                                    Button(action: {
+                                        Fetch().deletePayment(p: payment, h: house)
+                                    }, label: {
+                                        Text("Delete")
+                                            .foregroundColor(.red)
+                                        Image(systemName: "trash.fill")
+                                            .foregroundColor(.red)
+                                        
+                                    }
+                                    )
+                                })
+                                .padding(.bottom, -20)
+                        } else {
+                            ActivityAnnouncementCell(payment: .constant(payment))
+                                .padding(.bottom, -20)
+                        }
+                    } else if payment.isRequest {
                         
-                        ActivityPaymentCell(payment: .constant(payment))
-                            .contextMenu(menuItems: {
-                                Button(action: {
-                                    Fetch().deletePayment(p: payment, h: house)
-                                }, label: {
-                                    Text("Delete")
-                                    Image(systemName: "trash.fill")
-                                }
-                                )
-                            })
-                            .padding(.bottom, -20)
-                        
+                        if payment.by == UserDefaults.standard.string(forKey: "myId") || house.members.first(where: { (m) -> Bool in
+                            return m.id == UserDefaults.standard.string(forKey: "myId")
+                        })!.admin {
+                            ActivityRequestCell(payment: .constant(payment))
+                                .contextMenu(menuItems: {
+                                    Button(action: {
+                                        Fetch().deletePayment(p: payment, h: house)
+                                    }, label: {
+                                        Text("Delete")
+                                            .foregroundColor(.red)
+                                        Image(systemName: "trash.fill")
+                                            .foregroundColor(.red)
+                                        
+                                    }
+                                    )
+                                })
+                                .padding(.bottom, -20)
+                        } else {
+                            ActivityRequestCell(payment: .constant(payment))
+                                .padding(.bottom, -20)
+                        }
                     } else {
-                        ActivityPaymentCell(payment: .constant(payment))
-                            .padding(.bottom, -20)
                         
+                        if payment.by == UserDefaults.standard.string(forKey: "myId") || house.members.first(where: { (m) -> Bool in
+                            return m.id == UserDefaults.standard.string(forKey: "myId")
+                        })!.admin {
+                            
+                            ActivityPaymentCell(payment: .constant(payment))
+                                .contextMenu(menuItems: {
+                                    Button(action: {
+                                        Fetch().deletePayment(p: payment, h: house)
+                                    }, label: {
+                                        Text("Delete")
+                                        Image(systemName: "trash.fill")
+                                    }
+                                    )
+                                })
+                                .padding(.bottom, -20)
+                            
+                        } else {
+                            ActivityPaymentCell(payment: .constant(payment))
+                                .padding(.bottom, -20)
+                            
+                        }
                     }
                 }
             }
