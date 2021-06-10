a8d042e7d1f2f042282f17fa85489faa3110435b
Jacob Tepperman <jtepp@icloud.com>
2021-06-10 12:32:03 -0400

    added time ago

---
+++ split/Extensions/Date+timeAgo.swift
@@ -0,0 +1,19 @@
+//
+//  Date+timeAgo.swift
+//  split
+//
+//  Created by Jacob Tepperman on 2021-06-10.
+//
+
+import Foundation
+
+extension Date {
+    func timeAgo() -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .full
+        formatter.allowedUnits = [.year, .month, .day, .hour, .minute, .second]
+        formatter.zeroFormattingBehavior = .dropAll
+        formatter.maximumUnitCount = 1
+        return String(format: formatter.string(from: self, to: Date()) ?? “”, locale: .current)
+    }
+}
