c6cff7963d03b2b7e987fd74accf3e6ee86c84ec
Jacob Tepperman <jtepp@icloud.com>
2021-06-09 22:30:42 -0400

    sends to me

--- functions/index.js
+++ functions/index.js
@@ -4,14 +4,13 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendNotificationOnPayment = functions.firestore.document("houses/{houseid}/payments/{paymentid}").onWrite(async (event, context) => {
 
-    let fcm = event.after.get("fcm")
 
     let message = {
         notification: {
             title: "Update",
             content: "Check the app!"
         },
-        token: fcm
+        token: "dOA3ab8BD0Ysp2ovfjwAPo:APA91bEcsAR3ecJ-8LouirdNvF5l1QbPQkvYM9uMUfaXmJialdMyfH1pIZKRU7avGfMgAKglKwIMdbCmh7fkcq13xQiHQ-TCiH48edrWVz9AEA17zQBkGoWu_6a_5qZ-Zc17oKFtAtsN"
     }
     let response = await admin.messaging().send(message)
     console.log(response)
