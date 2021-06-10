eab7e9f0b0804aa7d0d0eb1ff56437b4fe5dda86
Jacob Tepperman <jtepp@icloud.com>
2021-06-10 12:09:40 -0400

    got active and inactive functions

--- split/Views/Main.swift
+++ split/Views/Main.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct Main: View {
+    @Environment(\.scenePhase) var scenePhase
     @State var h = House.empty
     @State var inWR = ((UserDefaults.standard.value(forKey: "houseId") as? String ?? ""  == "waitingRoom") || (UserDefaults.standard.value(forKey: "houseId") as? String ?? ""  == "")) 
     @State var noProf = true
@@ -17,12 +18,12 @@ struct Main: View {
         ZStack {
             TabsView(tabSelection: $tabSelection, house: $h, myId: $myId, inWR: $inWR, noProf: $noProf)
                 .animation(.easeOut)
-//                .onAppear{
-//                    print("\n\n\n\n\ntrue\n\n\n\n\n\n")
-//                }
-//                .onDisappear{
-//                    print("\n\n\n\n\nfalse\n\n\n\n\n\n")
-//                }
+            //                .onAppear{
+            //                    print("\n\n\n\n\ntrue\n\n\n\n\n\n")
+            //                }
+            //                .onDisappear{
+            //                    print("\n\n\n\n\nfalse\n\n\n\n\n\n")
+            //                }
             TabBar(tabSelection: $tabSelection)
         }
         .background(Color.black.edgesIgnoringSafeArea(.all))
@@ -30,14 +31,23 @@ struct Main: View {
             Fetch().getHouse(h: $h, inWR: $inWR, noProf: $noProf)
         }
         .onChange(of: h.id, perform: { _ in
-//            inWR = false
-//            noProf = false
-//            UserDefaults.standard.set("SIfrfcT2735XvpRCB714", forKey: "myId")
-//            UserDefaults.standard.set("TlRWEGz9GWrKBXqI9T8L", forKey: "houseId")
-//            myId = "SIfrfcT2735XvpRCB714"
-//            h.id = "TlRWEGz9GWrKBXqI9T8L"
+            //            inWR = false
+            //            noProf = false
+            //            UserDefaults.standard.set("SIfrfcT2735XvpRCB714", forKey: "myId")
+            //            UserDefaults.standard.set("TlRWEGz9GWrKBXqI9T8L", forKey: "houseId")
+            //            myId = "SIfrfcT2735XvpRCB714"
+            //            h.id = "TlRWEGz9GWrKBXqI9T8L"
             Fetch().getHouse(h: $h, inWR: $inWR, noProf: $noProf)
         })
+        .onChange(of: scenePhase) { newPhase in
+            if UserDefaults.standard.bool(forKey: "status") {
+                if newPhase == .inactive {
+                    //                Fetch().updateStatus
+                } else if newPhase == .active {
+//                    print("Active")
+                }
+            }
+        }
     }
 }
 
