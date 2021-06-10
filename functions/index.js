84c2a60005f86af40e468b62dea765bc83787aee
Jacob Tepperman <jtepp@icloud.com>
2021-06-09 22:50:15 -0400

    used new model before adding correct fcm

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,32 @@ admin.initializeApp(functions.config().firebase);
 
 exports.sendNotificationOnPayment = functions.firestore.document("houses/{houseid}/payments/{paymentid}").onWrite(async (event, context) => {
 
+    var title;
+    var body;
+
+    if (event.after.get("isAn")) {
+        title = "Announcement"
+        body = event.after.get("from") + " " + event.after.get("memo")
+    } else if (event.after.get("isRequest")) {
+        title = "Request received"
+        let reqFrom = event.after.get("reqFrom")
+        if (reqFrom.length > 1) {
+            let lo = (reqFrom.length - 1)
+            body = event.after.get("to") + " requested $" + event.after.get("amount").toFixed(2) + ", split you and " + lo + " other" + lo > 1 ? "s" : ""
+        } else {
+            body = event.after.get("to") + " requested $" + event.after.get("amount").toFixed(2) + "from you"
+        }
+    } else {
+        body = event.after.get("from") + " sent you $" + event.after.get("amount").toFixed(2)
+
+    }
+
+
 
     let message = {
         notification: {
-            title: "Update",
-            body: "Check the app!"
+            title: title,
+            body: body
         },
         token: "dOA3ab8BD0Ysp2ovfjwAPo:APA91bEcsAR3ecJ-8LouirdNvF5l1QbPQkvYM9uMUfaXmJialdMyfH1pIZKRU7avGfMgAKglKwIMdbCmh7fkcq13xQiHQ-TCiH48edrWVz9AEA17zQBkGoWu_6a_5qZ-Zc17oKFtAtsN"
     }
