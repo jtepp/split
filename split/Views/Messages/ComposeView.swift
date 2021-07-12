8d4a215f5f9a49e222c04d3bcf1274837fec8bf7
Jacob Tepperman <jtepp@icloud.com>
2021-07-11 21:07:14 -0400

    made taggedView

--- split/Views/Messages/ComposeView.swift
+++ split/Views/Messages/ComposeView.swift
@@ -10,24 +10,29 @@ import SwiftUI
 struct ComposeView: View {
     @State var msg = ""
     var body: some View {
-        HStack {
-            TextField("Message...", text: $msg, onEditingChanged: { bool in
-                print(bool)
-            }, onCommit: {
-                
-            })
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding()
-            Button("Send"){}
+        ZStack {
+            HStack {
+                TextField("Message...", text: $msg, onEditingChanged: { bool in
+                    print(bool)
+                }, onCommit: {
+                    
+                })
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                Button("Send"){
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }
                 .padding(.leading, -8)
                 .padding(.trailing)
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color("DarkMaterial"))
+            )
+            
+            .shadow(radius: 10)
+            
         }
-        .background(
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color("DarkMaterial"))
-        )
-        
-        .shadow(radius: 10)
     }
 }
 
@@ -44,5 +49,46 @@ struct ComposeView_Previews: PreviewProvider {
                 .padding()
         }
         .preferredColorScheme(.dark)
+    }
+}
+
+struct TaggedView: View {
+    @Binding var msg: String
+    @Binding var members: [Member]
+    var body: some View {
+        VStack {
+            ForEach(members.filter({ fm in
+                return fm.id != UserDefaults.standard.string(forKey: "myId") && fm.name.contains(msg)
+            })) {m in
+                HStack {
+                    b64toimg(b64: m.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 40, height: 40)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .shadow(radius: 4)
+                        .overlay(
+                            Image(systemName: "crown.fill")
+                                .offset(x: -3, y: -30)
+                                .scaleEffect(x: 1.2)
+                                .rotationEffect(.degrees(-30))
+                                .foregroundColor(Color.white.opacity(m.admin ? 1 : 0))
+                        )
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text(m.name)
+                            .bold()
+                    }
+                }
+                .foregroundColor(.primary)
+                .padding(.horizontal)
+                .padding(.vertical, 10)
+            }
+        }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color("DarkMaterial"))
+        )
     }
 }
