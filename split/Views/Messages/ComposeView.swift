f2912384b8b3fa44e7a5a31369e5c3180cb7a2c5
Jacob Tepperman <jtepp@icloud.com>
2021-07-11 21:19:10 -0400

    Set up compose view

--- split/Views/Messages/ComposeView.swift
+++ split/Views/Messages/ComposeView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ComposeView: View {
-    @State var msg = ""
+    @Binding var members: [Member]
+    @State var msg = "hey @joh"
+    @State var tagmsg = ""
+    @State var showTagged = false
     var body: some View {
         ZStack {
             HStack {
                 TextField("Message...", text: $msg, onEditingChanged: { bool in
-                    print(bool)
+                    
+                    if (msg.components(separatedBy: " ").last ?? "").contains("@") {
+                        tagmsg = msg.components(separatedBy: " ").last!
+                        withAnimation(.easeOut.speed(2)) {
+                            showTagged = true
+                        }
+                    } else {
+                        tagmsg = ""
+                        withAnimation(.easeOut.speed(2)) {
+                            showTagged = false
+                        }
+                    }
+                    
                 }, onCommit: {
                     
                 })
@@ -32,6 +47,11 @@ struct ComposeView: View {
             
             .shadow(radius: 10)
             
+            VStack {
+                TaggedView(tagmsg: $tagmsg, members: $members)
+                    .offset(y: showTagged ? 0: -600)
+                Spacer()
+            }
         }
     }
 }
@@ -45,50 +65,32 @@ struct ComposeView_Previews: PreviewProvider {
                 .font(.largeTitle)
                 .offset(y:-100)
                 .blur(radius: 10)
-            ComposeView()
+            ComposeView(members: .constant([.placeholder, .placeholder2, .placeholder3]))
                 .padding()
         }
-        .preferredColorScheme(.dark)
+        
     }
 }
 
 struct TaggedView: View {
-    @Binding var msg: String
+    @Binding var tagmsg: String
     @Binding var members: [Member]
     var body: some View {
         VStack {
             ForEach(members.filter({ fm in
-                return fm.id != UserDefaults.standard.string(forKey: "myId") && fm.name.contains(msg)
+                return fm.id != UserDefaults.standard.string(forKey: "myId") && fm.name.lowercased().contains(tagmsg.lowercased())
             })) {m in
-                HStack {
-                    b64toimg(b64: m.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 40, height: 40)
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                        .shadow(radius: 4)
-                        .overlay(
-                            Image(systemName: "crown.fill")
-                                .offset(x: -3, y: -30)
-                                .scaleEffect(x: 1.2)
-                                .rotationEffect(.degrees(-30))
-                                .foregroundColor(Color.white.opacity(m.admin ? 1 : 0))
-                        )
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Text(m.name)
-                            .bold()
+                Button {}
+                    label: {
+                        MemberCell(m: .constant(m))
                     }
-                }
-                .foregroundColor(.primary)
-                .padding(.horizontal)
-                .padding(.vertical, 10)
             }
         }
         .padding()
         .background(
-            RoundedRectangle(cornerRadius: 10)
+            RoundedRectangle(cornerRadius: 20)
                 .fill(Color("DarkMaterial"))
         )
+        .frame(minWidth: 200)
     }
 }
