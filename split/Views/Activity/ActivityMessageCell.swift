7b5d6e9a1a31298e9662a1d94b781d1d2357a0ab
Jacob Tepperman <jtepp@icloud.com>
2021-07-12 01:56:55 -0400

    using menus

--- split/Views/Activity/ActivityMessageCell.swift
+++ split/Views/Activity/ActivityMessageCell.swift
@@ -13,80 +13,37 @@ struct ActivityMessageCell: View {
     @Binding var member: Member
     @State var img = ""
     var body: some View {
-        HStack {
+        
+        Menu {
             
-            if payment.by != (UserDefaults.standard.string(forKey: "myId") ?? "") {
-                VStack {
-                    Spacer()
-                    if !nextMessageIsSameSender(allPayments, id: payment.id!, from: payment.from) {
-                        VStack {
-                            b64toimg(b64: img)
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 40, height: 40)
-                                .clipShape(RoundedRectangle(cornerRadius: 5))
-                                .shadow(radius: 4)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .fill(Color("Material"))
-                                )
-                                .onAppear {
-                                    Fetch().imgFromId(id: payment.by, img: $img)
-                                }
-                            Text(payment.from)
-                                .font(.caption2)
-                                .foregroundColor(.gray)
-                                .lineLimit(2)
-                                .padding(.vertical, -4)
-                                .minimumScaleFactor(0.4)
-                                .multilineTextAlignment(.center)
-                        }
-                        .fixedSize(horizontal: false, vertical: true)
-                        .frame(width: 40, height: 60)
-                    } else {
-                        Rectangle()
-                            .fill(Color.black)
-                            .frame(width: 40, height: 60)
-                    }
+            if payment.by != member.id {
+                Button {
+                    
+                } label: {
+                    Text("Reply")
+                    Image(systemName: "arrowshape.turn.up.left")
                 }
-                .padding(.trailing)
+                
             }
             
-            VStack {
-                HStack {
-                    //                        if payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") {
-                    //                            Spacer(minLength: 0)
-                    //                        }
-                    Text(payment.memo)
-                        .padding(.horizontal,10)
-                        .padding(.bottom, 10)
-                        .multilineTextAlignment(.leading)
-                        .foregroundColor(payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") ? .white : .black)
-                    //                        if payment.by != (UserDefaults.standard.string(forKey: "myId") ?? "") {
-                    Spacer(minLength: 0)
-                    //                        }
-                }
-                .lineLimit(10)
-                .fixedSize(horizontal: false, vertical: true)
+            if member.admin || payment.by == member.id {
+            Button {
+                var house = House.empty
+                house.id = member.home
+                Fetch().deletePayment(p: payment, h: house)
+            } label: {
+                Text("Delete")
+                    .foregroundColor(.red)
+                Image(systemName: "trash")
+                    .foregroundColor(.red)
             }
-            .padding(.vertical, 2)
-            .padding(.top, 6)
-            .padding(.bottom, 10)
-            .background(
-                CustomRoundedRect(corners: nextMessageIsSameSender(allPayments, id: payment.id!, from: payment.from) ? [.allCorners] : [.topLeft, .topRight, (payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") ? .bottomLeft : .bottomRight)],radius: 10)
-                    .fill(
-                        Color(payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") ? "MessageBlue" : "MessageWhite")
-                    )
-            )
-            .overlay(
-                VStack {
-                    Spacer()
-                    TimeBar(unix: payment.time, white: payment.by == (UserDefaults.standard.string(forKey: "myId") ?? ""))
-                        .padding(.horizontal,4)
-                }
-            )
-            if payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") {
-                VStack {
+            }
+            
+        }
+            label: {
+            HStack {
+                
+                if payment.by != (UserDefaults.standard.string(forKey: "myId") ?? "") {
                     VStack {
                         Spacer()
                         if !nextMessageIsSameSender(allPayments, id: payment.id!, from: payment.from) {
@@ -120,38 +77,110 @@ struct ActivityMessageCell: View {
                                 .frame(width: 40, height: 60)
                         }
                     }
-                    .padding(.leading)
+                    .padding(.trailing)
+                }
+                
+                VStack {
+                    HStack {
+                        //                        if payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") {
+                        //                            Spacer(minLength: 0)
+                        //                        }
+                        Text(payment.memo)
+                            .padding(.horizontal,10)
+                            .padding(.bottom, 10)
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") ? .white : .black)
+                        //                        if payment.by != (UserDefaults.standard.string(forKey: "myId") ?? "") {
+                        Spacer(minLength: 0)
+                        //                        }
+                    }
+                    .lineLimit(10)
+                    .fixedSize(horizontal: false, vertical: true)
+                }
+                .padding(.vertical, 2)
+                .padding(.top, 6)
+                .padding(.bottom, 10)
+                .background(
+                    CustomRoundedRect(corners: nextMessageIsSameSender(allPayments, id: payment.id!, from: payment.from) ? [.allCorners] : [.topLeft, .topRight, (payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") ? .bottomLeft : .bottomRight)],radius: 10)
+                        .fill(
+                            Color(payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") ? "MessageBlue" : "MessageWhite")
+                        )
+                )
+                .overlay(
+                    VStack {
+                        Spacer()
+                        TimeBar(unix: payment.time, white: payment.by == (UserDefaults.standard.string(forKey: "myId") ?? ""))
+                            .padding(.horizontal,4)
+                    }
+                )
+                if payment.by == (UserDefaults.standard.string(forKey: "myId") ?? "") {
+                    VStack {
+                        VStack {
+                            Spacer()
+                            if !nextMessageIsSameSender(allPayments, id: payment.id!, from: payment.from) {
+                                VStack {
+                                    b64toimg(b64: img)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 40, height: 40)
+                                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                                        .shadow(radius: 4)
+                                        .background(
+                                            RoundedRectangle(cornerRadius: 5)
+                                                .fill(Color("Material"))
+                                        )
+                                        .onAppear {
+                                            Fetch().imgFromId(id: payment.by, img: $img)
+                                        }
+                                    Text(payment.from)
+                                        .font(.caption2)
+                                        .foregroundColor(.gray)
+                                        .lineLimit(2)
+                                        .padding(.vertical, -4)
+                                        .minimumScaleFactor(0.4)
+                                        .multilineTextAlignment(.center)
+                                }
+                                .fixedSize(horizontal: false, vertical: true)
+                                .frame(width: 40, height: 60)
+                            } else {
+                                Rectangle()
+                                    .fill(Color.black)
+                                    .frame(width: 40, height: 60)
+                            }
+                        }
+                        .padding(.leading)
+                    }
                 }
             }
+    //        .contextMenu(menuItems: {
+    //
+    //                                    if payment.by != member.id {
+    //                                        Button {
+    //
+    //                                        } label: {
+    //                                            Text("Reply")
+    //                                            Image(systemName: "arrowshape.turn.up.left")
+    //                                        }
+    //
+    //                                    }
+    //
+    //                                    if member.admin || payment.by == member.id {
+    //                                    Button {
+    //                                        var house = House.empty
+    //                                        house.id = member.home
+    //                                        Fetch().deletePayment(p: payment, h: house)
+    //                                    } label: {
+    //                                        Text("Delete")
+    //                                            .foregroundColor(.red)
+    //                                        Image(systemName: "trash")
+    //                                            .foregroundColor(.red)
+    //                                    }
+    //                                    }
+    //
+    //                                })
+            .padding(.bottom, nextMessageIsSameSender(allPayments, id: payment.id!, from: payment.from) ? -20 : -8)
+            .padding(.top, -10)
         }
-        .contextMenu(menuItems: {
-                                    
-                                    if payment.by != member.id {
-                                        Button {
-                                            
-                                        } label: {
-                                            Text("Reply")
-                                            Image(systemName: "arrowshape.turn.up.left")
-                                        }
-                                        
-                                    }
-                                    
-                                    if member.admin || payment.by == member.id {
-                                    Button {
-                                        var house = House.empty
-                                        house.id = member.home
-                                        Fetch().deletePayment(p: payment, h: house)
-                                    } label: {
-                                        Text("Delete")
-                                            .foregroundColor(.red)
-                                        Image(systemName: "trash")
-                                            .foregroundColor(.red)
-                                    }
-                                    }
-                                    
-                                })
-        .padding(.bottom, nextMessageIsSameSender(allPayments, id: payment.id!, from: payment.from) ? -20 : -8)
-        .padding(.top, -10)
     }
 }
 
