19f1bafe91319d54e06fda564ecfaf7e9fd940e6
Jacob Tepperman <jtepp@icloud.com>
2021-12-18 23:20:09 -0500

    go back to onWrite
    This reverts commit 391595e2c53100cad68fe7ef3f1a4d37dba54527.
    
    Revert "different parameter in onCreate"
    
    This reverts commit 96c4f8481effc7ba4c27ffb3b81e92213e547de4.
    
    Revert "set notifications to on create not on write"
    
    This reverts commit 636af3dd7fad8b066d4ef9d0fc41889ddf06270b.

--- functions/index.js
+++ functions/index.js
@@ -2,77 +2,73 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
-exports.sendNotificationOnPayment = functions.firestore.document("houses/{houseid}/payments/{paymentid}").onCreate(async (documentSnapshot, context) => { // use onUpdate for opting
-    try {
-        var title;
-        var body;
-        var snd = "default"
-        const dataObject = documentSnapshot.data();
-        console.log("Payment created");
-        const type = dataObject["type"]
-        console.log("data fine");
-        if (dataObject["isAn"] || type == "announcement") {
-            let memo = dataObject["memo"]
-            title = "Announcement"
-            body = dataObject["from"] + " " + memo
-            if (memo.includes("join") || memo.includes("create")) {
-                snd = "join.mp3"
-            } else if (memo.includes("left") || memo.includes("remove")) {
-                snd = "leave.mp3"
-            } else if (memo.includes("Admin")) {
-                snd = "admin.mp3"
-            }
-        } else if (dataObject["isGM"] || type == "groupmessage") {
-            snd = "pay.mp3"
-            title = "from " + dataObject["from"]
-            body = dataObject["memo"]
-        } else if (dataObject["isRequest"] || type == "request") {
-            snd = "req.mp3"
-            title = "Request received"
-            let reqFrom = dataObject["reqfrom"]
-            console.log("REQFROM: " + reqFrom.toString())
-            if (reqFrom.length == 1) {
-                body = dataObject["to"] + " requested $" + Number(dataObject["amount"]).toFixed(2) + " from you"
-            } else if (reqFrom.length == 2) {
-                body = dataObject["to"] + " requested $" + Number(dataObject["amount"]).toFixed(2) + " ($" + Number(dataObject["amount"] / reqFrom.length).toFixed(2) + " each), split between you and 1 other"
-            } else {
-                body = dataObject["to"] + " requested $" + Number(dataObject["amount"]).toFixed(2) + " ($" + Number(dataObject["amount"] / reqFrom.length).toFixed(2) + " each), split between you and " + (reqFrom.length - 1) + " others"
-                // body = dataObject["to"] + " requested $" + Number(dataObject["amount"]).toFixed(2) + ", split between you and " + (reqFrom.length - 1) + " others"
-            }
+exports.sendNotificationOnPayment = functions.firestore.document("houses/{houseid}/payments/{paymentid}").onWrite(async (event, context) => {
+
+    var title;
+    var body;
+    var snd = "default"
+
+    if (event.after.get("isAn")) {
+        let memo = event.after.get("memo")
+        title = "Announcement"
+        body = event.after.get("from") + " " + memo
+        if (memo.includes("join") || memo.includes("create")) {
+            snd = "join.mp3"
+        } else if (memo.includes("left") || memo.includes("remove")) {
+            snd = "leave.mp3"
+        } else if (memo.includes("Admin")) {
+            snd = "admin.mp3"
+        }
+
+    } else if (event.after.get("isGM")) {
+        snd = "pay.mp3"
+        title = "from " + event.after.get("from")
+        body = event.after.get("memo")
+
+    } else if (event.after.get("isRequest")) {
+        snd = "req.mp3"
+        title = "Request received"
+        let reqFrom = event.after.get("reqfrom")
+        console.log("REQFROM: " + reqFrom.toString())
+        if (reqFrom.length == 1) {
+            body = event.after.get("to") + " requested $" + event.after.get("amount").toFixed(2) + " from you"
+        } else if (reqFrom.length == 2) {
+            body = event.after.get("to") + " requested $" + event.after.get("amount").toFixed(2) + ", split between you and " + (reqFrom.length - 1) + " other"
         } else {
-            snd = "pay.mp3"
-            title = "Payment received"
-            body = dataObject["from"] + " sent you $" + Number(dataObject["amount"]).toFixed(2)
-
+            body = event.after.get("to") + " requested $" + event.after.get("amount").toFixed(2) + ", split between you and " + (reqFrom.length - 1) + " others"
         }
+    } else {
+        snd = "pay.mp3"
+        title = "Payment received"
+        body = event.after.get("from") + " sent you $" + event.after.get("amount").toFixed(2)
 
-        if (!dataObject["isAn"] && !dataObject["isGM"]) {
-            if ((dataObject["memo"] || "") != "") {
-                body += " for " + dataObject["memo"]
-            }
+    }
+
+    if (!event.after.get("isAn") && !event.after.get("isGM")) {
+        if ((event.after.get("memo") || "") != "") {
+            body += " for " + event.after.get("memo")
         }
+    }
 
-        dataObject["fcm"].forEach(async tkn => {
-            let message = {
-                notification: {
-                    title: title,
-                    body: body
-                },
-                token: tkn,
-                apns: {
-                    payload: {
-                        aps: {
-                            sound: snd,
-                        }
+    event.after.get("fcm").forEach(async tkn => {
+        let message = {
+            notification: {
+                title: title,
+                body: body
+            },
+            token: tkn,
+            apns: {
+                payload: {
+                    aps: {
+                        sound: snd,
                     }
                 }
             }
-            let response = await admin.messaging().send(message)
-            console.log(response)
-        });
-    } catch (e) {
-        console.log(e)
-        console.log("ERROR: " + e)
-    }
+        }
+        let response = await admin.messaging().send(message)
+        console.log(response)
+    });
+
+
 
 })
