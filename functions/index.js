2a288b0b2e7ab1050568c1529eb8510d891f576e
Jacob Tepperman <jtepp@icloud.com>
2021-12-22 23:28:59 -0500

    hosting working and fixed so no edits still causes normal behaviour

--- functions/index.js
+++ functions/index.js
@@ -8,85 +8,152 @@ exports.sendNotificationOnPayment = functions.firestore.document("houses/{housei
     var body;
     var snd = "default"
     const type = event.after.get("type");
-
+    const edits = event.after.get("edits") || {}
+    const lastEdit = edits[Object.keys(edits).reverse()[0]]
+    let composer = lastEdit.match(/(.+?) (changed:|opted)/)[1]
+    let reqFrom = event.after.get("reqfrom")
+    let to = event.after.get("to") || ""
+    let from = event.after.get("from") || ""
     var typeFix = "unknown"
-    if (event.after.get("isAn") || type == "announcement") {
-        typeFix = "announcement"
-        event.after.get("id")
 
-        let memo = event.after.get("memo")
-        title = "Announcement"
-        body = event.after.get("from") + " " + memo
-        if (memo.includes("join") || memo.includes("create")) {
-            snd = "join.mp3"
-        } else if (memo.includes("left") || memo.includes("remove")) {
-            snd = "leave.mp3"
-        } else if (memo.includes("Admin")) {
-            snd = "admin.mp3"
-        }
+    if (Object.keys(edits).length > 0) { // announce edits
+        let fcms = []
+        await admin.firestore().collection("houses/" + context.params.houseid + "/members").get().then(function (querySnapshot) {
 
-    } else if (event.after.get("isGM") || type == "groupmessage") {
-        typeFix = "groupmessage"
-        snd = "pay.mp3"
-        title = "from " + event.after.get("from")
-        body = event.after.get("memo")
+            querySnapshot.forEach(function (doc) {
+                let data = doc.data()
+                if (type == "request" || event.after.get("isRequest")) { // request edit
+                    title = "Request Edited"
 
-    } else if (event.after.get("isRequest") || type == "request") {
-        typeFix = "request"
-        snd = "req.mp3"
-        title = "Request received"
-        let reqFrom = event.after.get("reqfrom")
-        let amount = event.after.get("amount").toFixed(2)
-        console.log("REQFROM: " + reqFrom.toString())
-        if (reqFrom.length == 1) {
-            body = event.after.get("to") + " requested $" + amount + " from you"
-        } else if (reqFrom.length == 2) {
-            body = event.after.get("to") + " requested $" + amount + " ($" + Number(amount / reqFrom.length).toFixed(2) + " each), split between you and 1 other"
-        } else {
-            body = event.after.get("to") + " requested $" + amount + " ($" + Number(amount / reqFrom.length).toFixed(2) + " each), split between you and " + (reqFrom.length - 1) + " others"
-        }
-    } else if (type != "unknown") {
-        typeFix = "payment"
-        snd = "pay.mp3"
-        title = "Payment received"
-        body = event.after.get("from") + " sent you $" + event.after.get("amount").toFixed(2)
+                    // fcms should be everyone in reqfrom
+                    if (reqFrom.includes(data.name)) {
+                        fcms.push(data.fcm)
+                        // also include to
+                    } else if (to == data.name) {
+                        fcms.push(data.fcm)
+                    }
+                } else { // payment edit
+                    title = "Payment Edited"
 
-    }
-
-    if (!event.after.get("isAn") && !event.after.get("isGM")) {
-        if ((event.after.get("memo") || "") != "") {
-            body += " for " + event.after.get("memo")
-        }
-    }
-
-
-
-    event.after.get("fcm").forEach(async tkn => {
-        let message = {
-            notification: {
-                title: title,
-                body: body
-            },
-            token: tkn,
-            apns: {
-                payload: {
-                    aps: {
-                        sound: snd,
+                    // fcms should be to
+                    if (to == data.name) {
+                        fcms.push(data.fcm)
+                        // also include from
+                    } else if (from == data.name) {
+                        fcms.push(data.fcm)
+                    }
+                }
+                // remove composer of edit
+                if (data.name.trim() == composer.trim()) {
+                    fcms = removeItemOnce(fcms, data.fcm)
+                }
+            })
+        })
+        fcms.forEach(async tkn => {
+            let message = {
+                notification: {
+                    title: title,
+                    body: lastEdit
+                },
+                token: tkn,
+                apns: {
+                    payload: {
+                        aps: {
+                            sound: "admin.mp3",
+                        }
                     }
                 }
             }
-        }
-        if (event.after.get("mute") != true) {
-            let response = await admin.messaging().send(message)
-            console.log(response)
-        }
-    });
+            if (tkn != undefined) {
+                let response = await admin.messaging().send(message)
+                console.log(response)
+            }
+        });
+    } else { // normal body
+        if (event.after.get("isAn") || type == "announcement") {
+            typeFix = "announcement"
+            event.after.get("id")
 
-    if (type == undefined) {
-        admin.firestore().doc("houses/" + context.params.houseid + "/payments/" + context.params.paymentid).update({
-            "type": typeFix,
-            "mute": true
-        })
+            let memo = event.after.get("memo")
+            title = "Announcement"
+            body = event.after.get("from") + " " + memo
+            if (memo.includes("join") || memo.includes("create")) {
+                snd = "join.mp3"
+            } else if (memo.includes("left") || memo.includes("remove")) {
+                snd = "leave.mp3"
+            } else if (memo.includes("Admin")) {
+                snd = "admin.mp3"
+            }
+
+        } else if (event.after.get("isGM") || type == "groupmessage") {
+            typeFix = "groupmessage"
+            snd = "pay.mp3"
+            title = "from " + event.after.get("from")
+            body = event.after.get("memo")
+
+        } else if (event.after.get("isRequest") || type == "request") {
+            typeFix = "request"
+            snd = "req.mp3"
+            title = "Request received"
+            let amount = event.after.get("amount").toFixed(2)
+            console.log("REQFROM: " + reqFrom.toString())
+            if (reqFrom.length == 1) {
+                body = event.after.get("to") + " requested $" + amount + " from you"
+            } else if (reqFrom.length == 2) {
+                body = event.after.get("to") + " requested $" + amount + " ($" + Number(amount / reqFrom.length).toFixed(2) + " each), split between you and 1 other"
+            } else {
+                body = event.after.get("to") + " requested $" + amount + " ($" + Number(amount / reqFrom.length).toFixed(2) + " each), split between you and " + (reqFrom.length - 1) + " others"
+            }
+        } else if (type != "unknown") {
+            typeFix = "payment"
+            snd = "pay.mp3"
+            title = "Payment received"
+            body = event.after.get("from") + " sent you $" + event.after.get("amount").toFixed(2)
+
+        }
+        if (!event.after.get("isAn") && !event.after.get("isGM")) { // add memo to body
+            if ((event.after.get("memo") || "") != "") {
+                body += " for " + event.after.get("memo")
+            }
+        }
+
+
+
+        event.after.get("fcm").forEach(async tkn => {
+            let message = {
+                notification: {
+                    title: title,
+                    body: body
+                },
+                token: tkn,
+                apns: {
+                    payload: {
+                        aps: {
+                            sound: snd,
+                        }
+                    }
+                }
+            }
+            if (!event.after.get("mute")) {
+                let response = await admin.messaging().send(message)
+                console.log(response)
+            }
+        });
+
+        if (type == undefined) {
+            admin.firestore().doc("houses/" + context.params.houseid + "/payments/" + context.params.paymentid).update({
+                "type": typeFix,
+                "mute": true
+            })
+        }
     }
 
 })
+
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
---
+++ spllit-online/public/firebase.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.firestore().doc('/foo/bar').get().then(() => { });
+    // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    // firebase.analytics(); // call to activate
+    // firebase.analytics().logEvent('tutorial_completed');
+    // firebase.performance(); // call to activate
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    try {
+        let app = firebase.app();
+        let features = [
+            'firestore',
+            'functions',
+            'analytics',
+            'performance',
+        ].filter(feature => typeof app[feature] === 'function');
+
+    } catch (e) {
+        console.error(e);
+    }
+});
