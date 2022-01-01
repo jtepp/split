556439f73f110f7fc04b1adb0bd972ad9059645b
Jacob Tepperman <jtepp@icloud.com>
2022-01-01 16:04:18 -0500

    used inheritance

--- spllit-online/public/js/celltest.js
+++ spllit-online/public/js/celltest.js
@@ -1,30 +1,20 @@
 class Post {
     constructor(data) {
         this.id = data.id;
-        this.amount = data.amount;
         this.by = data.by;
         this.fcm = data.fcm;
-        this.to = data.to;
         this.time = data.time;
         this.type = data.type;
         this.memo = data.memo;
-        this.reqfrom = data.reqfrom;
-        this.from = data.from;
-        this.edits = data.edits;
     }
 }
 
 class Payment {
     constructor(post) {
-        this.id = post.id;
+        Post.call(this, post)
         this.amount = post.amount;
-        this.by = post.by;
-        this.fcm = post.fcm;
         this.from = post.from;
-        this.memo = post.memo;
-        this.time = post.time;
         this.to = post.to;
-        this.type = post.type;
         this.edits = post.edits;
     }
 
@@ -32,40 +22,24 @@ class Payment {
 
 class Request {
     constructor(post) {
-        this.id = post.id;
+        Post.call(this, post)
         this.amount = post.amount;
-        this.by = post.by;
-        this.fcm = post.fcm;
         this.reqfrom = post.reqfrom;
-        this.memo = post.memo;
-        this.time = post.time;
         this.to = post.to;
-        this.type = post.type;
         this.edits = post.edits;
     }
 }
 
 class Announcement {
     constructor(post) {
-        this.id = post.id;
-        this.by = post.by;
+        Post.call(this, post)
         this.from = post.from;
-        this.memo = post.memo;
-        this.time = post.time;
-        this.type = post.type;
-        this.fcm = post.fcm;
     }
 }
 
 class Groupmessage {
     constructor(post) {
-        this.id = post.id;
-        this.by = post.by;
+        Post.call(this, post)
         this.from = post.from;
-        this.memo = post.memo;
-        this.time = post.time;
-        this.type = post.type;
-        this.fcm = post.fcm;
     }
-
 }
